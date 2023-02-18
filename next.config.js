5511099519b327286200ed23159c9f858b1947b5
Jairwin <jairwin@jairwin.cn>
2023-02-18 21:42:21 +0800

    feat: add less

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
+// use less: https://github.com/SolidZORO/next-plugin-antd-less
+const withAntdLess = require('next-plugin-antd-less');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    transpilePackages: ['antd-mobile'],
+  },
+  ...withAntdLess({
+    modifyVars: {
+      '@primary-color': '#2598ff',
+      '@detail-color': '#2db7f5',
+      '@edit-color': '#8354ee',
+      '@danger-color': '#ff3141',
+      '@white': '#fff',
+      '@default-black': '#333',
+      '@pink': '#ffc0cb',
+      '@divide-color': '#f1f5ff',
+      '@text-color': '#999',
+      '@theme-color': '#1677ff',
+      '@warn-color': '#ff3141',
+      '@gray': '#7a7a7a',
+      '@default-font-size': '16px',
+      '@common-font-size': '14px',
+      '@small-font-size': '12px',
+    },
+  }),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <>Nextjs</>;
+import { Button } from 'antd-mobile';
+require('./index.less');
+
+export default function Main() {
+  return (
+    <div className="main">
+      antd-mobile
+      <Button color="primary" size="middle">
+        Button
+      </Button>
+    </div>
+  );
 }
