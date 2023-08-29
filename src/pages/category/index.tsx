2d25f6441a14a8c17f8f1ed3e9ff04ca8bd07108
Jairwin <jairwin@jairwin.cn>
2023-08-29 15:46:39 +0800

    chore: for test

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -9,7 +9,7 @@ import style from './index.module.less';
 export async function getServerSideProps(props: any) {
   // TODO:for test
   const { res } = props;
-  res.setHeader('Cache-Control', 'max-age=86400, stale-while-revalidate=86400');
+  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate=86400');
   const resp = await queryList();
   return {
     props: resp,
